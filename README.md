11af3bc941534527acfaaba0da6fca517013863b
Aaron Hardy <ashtarcommunications@gmail.com>
2023-03-22 08:30:40 -0600

    Packages

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-#Ashtar's dotfiles
+# Ashtar's dotfiles
 
-Personalized dotfils, vim plugins, etc.
+Personalized dotfiles, vim plugins, etc.
 
 Help from:
 
 https://dotfiles.github.io/
 
 http://vimawesome.com
+
+Install with .dotfiles/install.sh and .dotfiles/install-packages.sh
---
+++ install-packages.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+apt-get install -y awscli \
+    buildah \
+    build-essential \
+    code \
+    curl \
+    ffmpeg \
+    fzf \
+    git \
+    inkscape \
+    jq \
+    jxplorer \
+    mysql-workbench-community \
+    net-tools \
+    obs-studio \
+    openvpn \
+    openvpn-systemd-resolved \
+    packer \
+    pandoc \
+    podman \
+    powertop \
+    rename \
+    tailscale \
+    terraform \
+    vagrant \
+    vim \
+    virtualbox \
+    youtube-dl \
+    yt-dlp \
+    zeal \
+    zoom
+
