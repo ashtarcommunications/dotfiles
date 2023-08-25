c201d33d2fd4f18a9fb5704f2f92a45865d17f31
Aaron Hardy <ashtarcommunications@gmail.com>
2023-08-24 20:20:18 -0600

    Packages

--- install-packages.sh
+++ install-packages.sh
@@ -2,31 +2,51 @@
 
 apt-get install -y \
     awscli \
+    binutils \
     buildah \
     build-essential \
+    cifs-utils \
+    cloc \
     code \
     curl \
+    dropbox \
+    fastboot \
     ffmpeg \
     fzf \
     git \
+    glimpse \
+    google-chrome-stable \
+    gparted \
     inkscape \
+    ivpn-ui \
     jq \
     jxplorer \
+    kde-plasma-desktop \
+    lxc \
+    make \
+    mariadb-server \
+    moreutils \
+    mtp-tools \
     mysql-workbench-community \
     net-tools \
     obs-studio \
-    openvpn \
-    openvpn-systemd-resolved \
     packer \
     pandoc \
     podman \
     powertop \
+    python3-pip \
+    python-is-python3 \
+    qemu-user-static \
+    qemu-utils \
     rename \
+    sane \
+    screen \
     tailscale \
     terraform \
     vagrant \
     vim \
     virtualbox \
+    wireguard \
     yt-dlp \
     zeal \
     zoom
